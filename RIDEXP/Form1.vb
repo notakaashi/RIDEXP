35507b1448db6f2771cbed61d6b03baee61be072
notJ1L <caisipjonel29@gmail.com>
2025-07-24 00:52:02 +0800

    -- ADMIN PAGE

--- RIDEXP/Form1.vb
+++ RIDEXP/Form1.vb
@@ -82,6 +82,11 @@
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         FORMRENTAL_STEP3.Show()
+        Hide()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        FORM_ADMIN.Show()
         Me.Hide()
     End Sub
 End Class
